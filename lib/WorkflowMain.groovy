f19bfaa328eef802d88c82daadfac1b82978016c
patrick <patrick.demarta@gmail.com>
2025-10-30 16:27:25 +0100

    fix missing Java dependencies in NfcoreSchema.groovy.

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -1,5 +1,4 @@
-// This file is based on the nf-core/tools pipeline-template.
-// Changes to this file must be propagated via wf-template.
+// Simplified WorkflowMain for wf-ubcg2
 
 class WorkflowMain {
 
@@ -12,12 +11,15 @@ class WorkflowMain {
 
     // Generate help string
     public static String help(workflow, params, log) {
-        String line_sep = ' \\ \n\t'
-        String command_example = params.wf.example_cmd.join(line_sep)
-        String command = 'nextflow run ' + workflow.manifest.name + line_sep + command_example
         String help_string = ''
         help_string += NfcoreTemplate.logo(workflow, params.monochrome_logs)
-        help_string += NfcoreSchema.paramsHelp(workflow, params, command)
+        help_string += "\nTypical pipeline command:\n\n"
+        help_string += "  nextflow run ${workflow.manifest.name} \\\n"
+        params.wf.example_cmd.each { cmd ->
+            if (cmd && cmd.trim()) {
+                help_string += "    ${cmd} \\\n"
+            }
+        }
         help_string += '\n' + citation(workflow) + '\n'
         return help_string
     }
@@ -27,10 +29,20 @@ class WorkflowMain {
         String workflow_version = NfcoreTemplate.version(workflow)
         String summary_log = ''
         summary_log += NfcoreTemplate.logo(workflow, params.monochrome_logs)
-        summary_log += NfcoreSchema.paramsSummaryLog(workflow, params)
-        summary_log += '\n' + citation(workflow) + '\n'
         summary_log += NfcoreTemplate.dashedLine(params.monochrome_logs)
-        summary_log += "\nThis is ${workflow.manifest.name} ${workflow_version}.\n"
+        summary_log += "\nWorkflow: ${workflow.manifest.name} ${workflow_version}\n"
+        summary_log += "Run Name: ${workflow.runName}\n"
+        summary_log += NfcoreTemplate.dashedLine(params.monochrome_logs)
+        summary_log += "\nParameters:\n"
+
+        // Show main parameters
+        if (params.fasta) summary_log += "  --fasta: ${params.fasta}\n"
+        if (params.ucg_dir) summary_log += "  --ucg_dir: ${params.ucg_dir}\n"
+        summary_log += "  --out_dir: ${params.out_dir}\n"
+        if (params.mafft_threads) summary_log += "  --mafft_threads: ${params.mafft_threads}\n"
+
+        summary_log += NfcoreTemplate.dashedLine(params.monochrome_logs)
+        summary_log += '\n' + citation(workflow) + '\n'
         summary_log += NfcoreTemplate.dashedLine(params.monochrome_logs)
         return summary_log
     }
@@ -51,19 +63,12 @@ class WorkflowMain {
         }
 
         // Explode on conda
-        // conda.enabled seems to be backward compatible but wrap this
-        // in a generic catch just in case
         try {
             if (workflow.session.config.conda.enabled) {
                 log.error "Sorry, this workflow is not compatible with Conda, please use -profile standard (Docker) or -profile singularity."
                 System.exit(1)
             }
         } catch(Exception e) {}
-
-        // Validate workflow parameters via the JSON schema
-        if (params.validate_params) {
-            NfcoreSchema.validateParameters(workflow, params, log)
-        }
 
         // Print parameter summary log to screen
         log.info paramsSummaryLog(workflow, params, log)
